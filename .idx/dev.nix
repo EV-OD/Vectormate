8801c5149dac9829ca01dea4c80001649bb500d0
Evod lc <evod599@gmail.com>
2025-06-24 06:19:58 +0000

    dev

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,6 +7,8 @@
   packages = [
     pkgs.nodejs_20
     pkgs.zulu
+    pkgs.openssh          # SSH client & tools (ssh-keygen, ssh-agent)
+    pkgs.gh
   ];
   # Sets environment variables in the workspace
   env = {};
